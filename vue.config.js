55b685e1621f39d14c5e1cbfc3c7a455ed1bb19e
shoumen <0showman0@gmail.com>
2022-07-03 11:01:09 +0300

    Experiments with serving

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    writeToDisk: true
+    devMiddleware: {
+      writeToDisk: true
+    }
   },
 })
