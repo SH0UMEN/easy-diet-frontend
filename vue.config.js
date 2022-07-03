91402031830cfb45aa1d29d86b25ebba173d13a2
shoumen <0showman0@gmail.com>
2022-07-03 18:47:44 +0300

    I MADE IT! I CONNECTED DJANGO AND VUE!

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  assetsDir: 'static',
+
   devServer: {
     devMiddleware: {
-      writeToDisk: true
+      writeToDisk: true,
     }
   },
+
+  configureWebpack: {
+    output: {
+      hotUpdateMainFilename: 'static/[runtime].[fullhash].hot-update.json',
+      hotUpdateChunkFilename: 'static/[id].[fullhash].hot-update.js',
+    },
+  }
 })
