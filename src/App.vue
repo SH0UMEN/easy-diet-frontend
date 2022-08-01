e93fc787f63d115a17f012b81c40be2b578dc329
shoumen <0showman0@gmail.com>
2022-08-01 21:21:55 +0300

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,30 @@
 						<v-btn rounded="pill" variant="plain">Меню</v-btn>
 						<v-btn rounded="pill" variant="plain">Блюда</v-btn>
 					</v-col>
+
 					<v-divider class="my-5" vertical></v-divider>
+
 					<v-col class="d-flex align-center justify-center py-0" cols="1">
 						<v-img max-height="44" :src="require('@/assets/food-cheese-stinky.svg')"/>
 					</v-col>
+
 					<v-divider class="my-5" vertical></v-divider>
+
 					<v-col class="d-flex align-center">
 						<div v-if="user != null" class="d-flex align-center">
 							<v-avatar class="mx-2" color="indigo" size="32"></v-avatar>
+
 							<span>{{ user.username }}</span>
+
+							<v-btn rounded="pill" variant="plain">Выйти</v-btn>
 						</div>
+
+						<v-btn></v-btn>
 					</v-col>
 				</v-row>
 			</v-container>
 		</v-app-bar>
+
 		<v-main>
 			<router-view/>
 		</v-main>
