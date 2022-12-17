f6531be2fc9a6716bf3e914ba0fdf12544810cd8
shoumen <0showman0@gmail.com>
2022-12-17 10:45:22 +0300

    Done dish creation

--- src/pages/dishes/Create.vue
+++ src/pages/dishes/Create.vue
@@ -1,22 +1,24 @@
 <template>
 	<v-responsive max-width="768" class="mx-auto px-2 pt-4 pt-sm-12">
 		<v-container>
-			<v-form @submit.prevent="onSubmit">
+			<v-form @submit.prevent="onSubmit" v-model="valid">
 				<h2 class="text-center mb-6">{{ t('dishes.create.title') }}</h2>
-				<v-text-field v-model="dish.title" :label="t('dishes.create.form.title')" variant="solo"></v-text-field>
+				<v-text-field v-model="dish.title" :rules="titleRules" :label="t('dishes.create.form.title')" variant="solo" class="mb-2"></v-text-field>
 				<v-file-input :label="t('dishes.create.form.image')"
 							  @update:modelValue="onImageSelected"
+							  :rules="imageRules"
 							  accept="image/png,image/jpeg,image/bmp"
 							  prepend-icon="mdi-camera"
 							  variant="solo"
+							  class="mb-2"
 							  outlined
 							  dense>
 				</v-file-input>
 
-				<product-selector :label="t('dishes.create.form.product.label')" @selected="onProductSelected"></product-selector>
+				<product-selector :label="t('dishes.create.form.product.label')" @selected="onProductSelected" class="mb-2"></product-selector>
 
 				<v-row>
-					<v-col v-for="relation in dish.dishProductRelations" cols="12" sm="6" class="grow">
+					<v-col v-for="relation in dish.dishProductRelations" cols="12" sm="6" class="grow mb-2">
 						<v-card class="fill-height">
 							<v-card-text class="d-flex flex-column fill-height">
 								<span class="text-subtitle-2">{{ relation.product.i18n[$i18n.locale].title }}</span>
@@ -35,12 +37,16 @@
 					</v-col>
 				</v-row>
 
-				<v-btn type="submit"
+				<v-btn :disabled="!valid || dish.dishProductRelations.length == 0 || loading"
+					   :loading="loading"
+					   type="submit"
 					   class="mt-6"
 					   color="yellow-accent-3"
 					   block>
 					{{ t('dishes.create.form.submit') }}
 				</v-btn>
+
+				<v-alert class="mt-6" v-if="error != null" type="error">{{ error }}</v-alert>
 			</v-form>
 		</v-container>
 	</v-responsive>
@@ -49,14 +55,22 @@
 <script setup lang="ts">
 	import ProductSelector from '@/components/forms/controls/ProductSelector.vue';
 	import { useI18n } from 'vue-i18n';
-	import { reactive } from 'vue';
+	import { reactive, ref } from 'vue';
+	import ValidationService from '@/services/validation';
 	import DishService from '@/services/dish';
 	import Product from '@/models/product';
 	import Dish from '@/models/dish';
-
-	const dish = reactive<Dish>({ title: '', image: null, dishProductRelations: [] });
+	import router from '@/router';
 
 	const { t } = useI18n();
+
+	const dish = reactive<Dish>({ title: '', image: null, dishProductRelations: [] });
+	const titleRules = reactive([ValidationService.required(t)]);
+	const imageRules = reactive([ValidationService.requiredFile(t)]);
+
+	const valid = ref(false);
+	const loading = ref(false);
+	const error = ref<string | null>(null);
 
 	const onProductSelected = (value: Product) => {
 		dish.dishProductRelations.push({ product: value, grams: 0 });
@@ -67,6 +81,14 @@
 	};
 
 	const onSubmit = async () => {
-		await new DishService().post(dish);
+		loading.value = true;
+		try {
+			await new DishService().post(dish);
+			router.push({ name: 'dishes-mine' });
+		} catch(e) {
+			error.value = 'Неизвестная ошибка';
+		} finally {
+			loading.value = false;
+		}
 	};
 </script>
--- src/services/validation.ts
+++ src/services/validation.ts
@@ -1,7 +1,7 @@
 import { ComposerTranslation } from 'vue-i18n';
 
 class ValidationService {
-	static username(t: ComposerTranslation): Array<Function> {
+	public static username(t: ComposerTranslation): Array<Function> {
 		return [
 			ValidationService.required(t),
 			ValidationService.moreThanOrEqualTo(t, 5),
@@ -9,7 +9,7 @@ class ValidationService {
 		];
 	}
 
-	static password(t: ComposerTranslation): Array<Function> {
+	public static password(t: ComposerTranslation): Array<Function> {
 		return [
 			ValidationService.required(t),
 			ValidationService.moreThanOrEqualTo(t, 6),
@@ -17,15 +17,19 @@ class ValidationService {
 		];
 	}
 
-	private static required(t: ComposerTranslation): Function {
-		return (v: string) => v != '' || t('form.required')
+	public static required(t: ComposerTranslation): Function {
+		return (v: string) => v != '' || t('form.required');
 	}
 
-	private static lessThanOrEqualTo(t: ComposerTranslation, max: number): Function {
+	public static requiredFile(t: ComposerTranslation): Function {
+		return (v: Array<Blob>) => v.length > 0 || t('form.required');
+	}
+
+	public static lessThanOrEqualTo(t: ComposerTranslation, max: number): Function {
 		return (v: string) => v.length <= max || t('form.lessThanOrEqualTo', { max: max });
 	}
 
-	private static moreThanOrEqualTo(t: ComposerTranslation, min: number): Function {
+	public static moreThanOrEqualTo(t: ComposerTranslation, min: number): Function {
 		return (v: string) => v.length >= min ||  t('form.moreThanOrEqualTo', { min: min });
 	}
 }
